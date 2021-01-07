a8dbda1541869f7958355beaeea1ac124e4a08ff
Denis <denis.97@list.ru>
2021-01-07 20:17:49 +0300

    Fix caching with spring boot web

--- src/main/java/com/sothawo/mapjfx/offline/OfflineCache.java
+++ src/main/java/com/sothawo/mapjfx/offline/OfflineCache.java
@@ -25,9 +25,11 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.UnsupportedEncodingException;
+import java.lang.reflect.Field;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.net.URLStreamHandlerFactory;
 import java.nio.file.FileSystems;
 import java.nio.file.FileVisitResult;
 import java.nio.file.Files;
@@ -47,9 +49,9 @@ import java.util.stream.Collectors;
  * https resource is intercepted. If the result of the same call is already stored in the local cache directory, it is
  * returned without a further connect to the network. If it is not in the cache directory, a network request is made and
  * the returned data is stored in the local cache directory.
- *
+ * <p>
  * The cache is implemented as singleton.
- *
+ * <p>
  * A list of regexp strings can be set with {@link #setNoCacheFilters(Collection)}. URLs that match any of these
  * patterns will not be cached.
  *
@@ -58,27 +60,40 @@ import java.util.stream.Collectors;
 public enum OfflineCache {
     INSTANCE;
 
-    /** Logger for the class */
+    /**
+     * Logger for the class
+     */
     private static final Logger logger = LoggerFactory.getLogger(OfflineCache.class);
-    /** the url pattern to be mapped. */
+    /**
+     * the url pattern to be mapped.
+     */
     private static final String TILE_OPENSTREETMAP_ORG = "[a-z]\\.tile\\.openstreetmap\\.org";
     private static final int PRELOAD_DATABUFFER_SIZE = 1024 * 1024;
-    /** list of Patterns that are used to match against urls to prevent caching. */
+    /**
+     * list of Patterns that are used to match against urls to prevent caching.
+     */
     private final Collection<Pattern> noCachePatterns = new ArrayList<>();
-    /** list of Patterns that are used to match against urls to specify caching. */
+    /**
+     * list of Patterns that are used to match against urls to specify caching.
+     */
     private final Collection<Pattern> cachePatterns = new ArrayList<>();
-    /** flag if the URLStreamHandlerfactory is initialized */
+    /**
+     * flag if the URLStreamHandlerfactory is initialized
+     */
     private boolean urlStreamHandlerFactoryIsInitialized = false;
-    /** flag if the cache is active. */
+    /**
+     * flag if the cache is active.
+     */
     private boolean active = false;
-    /** the cache directory. */
+    /**
+     * the cache directory.
+     */
     private Path cacheDirectory;
 
     /**
      * helper method to recursively delete all files in a directory and the directory itself.
      *
-     * @param path
-     *     the directory to delete
+     * @param path the directory to delete
      */
     static void clearDirectory(final Path path) throws IOException {
         Files.walkFileTree(path, new DeletingFileVisitor(path));
@@ -96,8 +111,7 @@ public enum OfflineCache {
     /**
      * Sets a list of Java pattern strings to define which URLs should be cached. Only URLs that do match one of the given patterns are cached.
      *
-     * @param cacheFilters
-     *     the patterns defining what to cache.
+     * @param cacheFilters the patterns defining what to cache.
      */
     public void setCacheFilters(final Collection<String> cacheFilters) {
         if (!noCachePatterns.isEmpty()) {
@@ -112,8 +126,7 @@ public enum OfflineCache {
     /**
      * Sets a list of Java pattern strings to define which URLs should not be cached. URLs that do match any of the given patterns are not cached.
      *
-     * @param noCacheFilters
-     *     the patterns defining what not to cache.
+     * @param noCacheFilters the patterns defining what not to cache.
      */
     public void setNoCacheFilters(final Collection<String> noCacheFilters) {
         if (!cachePatterns.isEmpty()) {
@@ -132,12 +145,9 @@ public enum OfflineCache {
     /**
      * sets the cacheDirectory.
      *
-     * @param cacheDirectory
-     *     the new directory
-     * @throws NullPointerException
-     *     if cacheDirectory is null
-     * @throws IllegalArgumentException
-     *     if cacheDirectory does not exist or is not writeable
+     * @param cacheDirectory the new directory
+     * @throws NullPointerException     if cacheDirectory is null
+     * @throws IllegalArgumentException if cacheDirectory does not exist or is not writeable
      */
     public void setCacheDirectory(final Path cacheDirectory) {
         final Path dir = Objects.requireNonNull(cacheDirectory);
@@ -150,12 +160,9 @@ public enum OfflineCache {
     /**
      * sets the cacheDirectory.
      *
-     * @param cacheDirectory
-     *     the new directory
-     * @throws NullPointerException
-     *     if cacheDirectory is null
-     * @throws IllegalArgumentException
-     *     if cacheDirectory does not exist or is not writeable
+     * @param cacheDirectory the new directory
+     * @throws NullPointerException     if cacheDirectory is null
+     * @throws IllegalArgumentException if cacheDirectory does not exist or is not writeable
      */
     public void setCacheDirectory(final String cacheDirectory) {
         setCacheDirectory(FileSystems.getDefault().getPath(Objects.requireNonNull(cacheDirectory)));
@@ -164,8 +171,7 @@ public enum OfflineCache {
     /**
      * checks whether a URL should be cached at all.
      *
-     * @param u
-     *     the URL to check
+     * @param u the URL to check
      * @return true if the URL should be cached.
      */
     boolean urlShouldBeCached(final URL u) {
@@ -173,12 +179,12 @@ public enum OfflineCache {
 
         if (!noCachePatterns.isEmpty()) {
             return noCachePatterns.stream()
-                .noneMatch(pattern -> pattern.matcher(urlString).matches());
+                    .noneMatch(pattern -> pattern.matcher(urlString).matches());
         }
 
         if (!cachePatterns.isEmpty()) {
             return cachePatterns.stream()
-                .anyMatch(pattern -> pattern.matcher(urlString).matches());
+                    .anyMatch(pattern -> pattern.matcher(urlString).matches());
         }
 
         return true;
@@ -191,12 +197,9 @@ public enum OfflineCache {
     /**
      * sets the active state of the cache
      *
-     * @param active
-     *     new state
-     * @throws IllegalArgumentException
-     *     if active is true and no cacheDirectory has been set.
-     * @throws IllegalStateException
-     *     if the factory cannot be initialized.
+     * @param active new state
+     * @throws IllegalArgumentException if active is true and no cacheDirectory has been set.
+     * @throws IllegalStateException    if the factory cannot be initialized.
      */
     public void setActive(final boolean active) {
         if (active && null == cacheDirectory) {
@@ -211,14 +214,26 @@ public enum OfflineCache {
     /**
      * sets up the URLStreamHandlerFactory.
      *
-     * @throws IllegalStateException
-     *     if the factory cannot be initialized.
+     * @throws IllegalStateException if the factory cannot be initialized.
      */
     private void setupURLStreamHandlerFactory() {
         if (!urlStreamHandlerFactoryIsInitialized) {
             final String msg;
             try {
-                URL.setURLStreamHandlerFactory(new CachingURLStreamHandlerFactory(this));
+                Field factoryField = URL.class.getDeclaredField("factory");
+                factoryField.setAccessible(true);
+                URLStreamHandlerFactory urlStreamHandlerFactory
+                        = (URLStreamHandlerFactory) factoryField.get(null);
+                if (urlStreamHandlerFactory == null) {
+                    URL.setURLStreamHandlerFactory(new CachingURLStreamHandlerFactory(this));
+                } else {
+                    Field lockField = URL.class.getDeclaredField("streamHandlerLock");
+                    lockField.setAccessible(true);
+                    synchronized (lockField.get(null)) {
+                        factoryField.set(null, null);
+                        URL.setURLStreamHandlerFactory(new CachingURLStreamHandlerFactory(this));
+                    }
+                }
                 urlStreamHandlerFactoryIsInitialized = true;
                 return;
             } catch (final Error e) {
@@ -231,6 +246,8 @@ public enum OfflineCache {
                 if (logger.isErrorEnabled()) {
                     logger.error(msg);
                 }
+            } catch (Exception e) {
+                throw new RuntimeException(e);
             }
             throw new IllegalStateException(msg);
         }
@@ -239,8 +256,7 @@ public enum OfflineCache {
     /**
      * check wether an URL is cached.
      *
-     * @param url
-     *     the URL to check
+     * @param url the URL to check
      * @return true if cached
      */
     boolean isCached(final URL url) {
@@ -258,15 +274,11 @@ public enum OfflineCache {
     /**
      * returns the filename path for a cachefile
      *
-     * @param url
-     *     the url to store in the cache
+     * @param url the url to store in the cache
      * @return the filename path for the url
-     * @throws IllegalStateException
-     *     if no cacheDirectory is set.
-     * @throws UnsupportedEncodingException
-     *     if the url cannot be UTF-8
-     * @throws NullPointerException
-     *     if url or it's external form is null
+     * @throws IllegalStateException        if no cacheDirectory is set.
+     * @throws UnsupportedEncodingException if the url cannot be UTF-8
+     * @throws NullPointerException         if url or it's external form is null
      */
     Path filenameForURL(final URL url) throws UnsupportedEncodingException {
         if (null == cacheDirectory) {
@@ -281,8 +293,7 @@ public enum OfflineCache {
      * do some special mappings, i.e. map the openstreetmap urls with [a,b,c].tile.openstreetmap.org to x.tile
      * .openstreetmap.org.
      *
-     * @param urlString
-     *     the string to map
+     * @param urlString the string to map
      * @return the mapped string
      */
     private String doMappings(final String urlString) {
@@ -296,10 +307,8 @@ public enum OfflineCache {
     /**
      * writes the datainfo for a cache file.
      *
-     * @param cacheFile
-     *     the cache file
-     * @param cachedDataInfo
-     *     the data info
+     * @param cacheFile      the cache file
+     * @param cachedDataInfo the data info
      */
     void saveCachedDataInfo(final Path cacheFile, final CachedDataInfo cachedDataInfo) {
         final Path cacheDataFile = Paths.get(cacheFile + ".dataInfo");
@@ -320,8 +329,7 @@ public enum OfflineCache {
     /**
      * reads the cached data info for a cache file
      *
-     * @param cacheFile
-     *     the cache file
+     * @param cacheFile the cache file
      * @return the cached data info.
      */
     CachedDataInfo readCachedDataInfo(final Path cacheFile) {
@@ -362,10 +370,8 @@ public enum OfflineCache {
      * loads the given URL and so puts them into the cache. If the cache is disabled, the call is ignored.
      * Any errors during load are logged and further ignored.
      *
-     * @param urls
-     *     the list of URLs
-     * @param parallelism
-     *     the number of threads to use for loading. If set to zero or below, the number of available processors is used.
+     * @param urls        the list of URLs
+     * @param parallelism the number of threads to use for loading. If set to zero or below, the number of available processors is used.
      */
     public void preloadURLs(final Collection<String> urls, int parallelism) {
 
@@ -400,7 +406,9 @@ public enum OfflineCache {
      * class to recursivly delete files.
      */
     private static class DeletingFileVisitor extends SimpleFileVisitor<Path> {
-        /** the top directory, this will not be deleted. */
+        /**
+         * the top directory, this will not be deleted.
+         */
         private final Path rootDir;
 
         public DeletingFileVisitor(final Path path) {
